077c019053a6ae124c01cb784ef107e2eeaf2425
BAI29 <1244283683@qq.com>
2020-09-02 22:31:50 +0800

    版本1添加点击动画

--- js/data.js
+++ js/data.js
@@ -3,23 +3,30 @@ function getMethodarea() {
     var classname = '';
     var method = '';
 
-    $.ajax({
-        url : "http://localhost:8080/methodarea",
-        type : "get",
-        async : false,
-        dataType : "json",
-        success : function(result) {
-            console.log(result)
-            classname = result.classname;
-            method = result.method;
-            $('#classname').html(classname);
-            $('#method').html(method);
-        },
-        error : function(e){
-            console.log(e.status);
-            console.log(e.responseText);
-        }
-    });
+    // $.ajax({
+    //     url : "http://localhost:8080/methodarea",
+    //     type : "get",
+    //     async : false,
+    //     dataType : "json",
+    //     success : function(result) {
+    //         console.log(result)
+    //         classname = result.classname;
+    //         method = result.method;
+    //         $('#classname').html(classname);
+    //         $('#method').html(method);
+    //     },
+    //     error : function(e){
+    //         console.log(e.status);
+    //         console.log(e.responseText);
+    //     }
+    // });
+
+    setTimeout(function () {
+        $('.box5').css('display', 'block');//1出现
+    }, 500);
+    setTimeout(function () {
+        $('.box5').css('margin-top', '180%');//1下降
+    }, 2000);
 }
 
 function getHeap() {
@@ -27,24 +34,31 @@ function getHeap() {
     var name = '';
     var value = '';
     var quote = '';
-    $.ajax({
-        url : "http://localhost:8080/heap",
-        type : "get",
-        async : false,
-        dataType : "json",
-        success : function(result) {
-            console.log(result)
-            name = result.name;
-            value = result.value;
-            $('#name').html(name);
-            $('#value').html(value)
-            $('#quote').html(quote);
-        },
-        error : function(e){
-            console.log(e.status);
-            console.log(e.responseText);
-        }
-    });
+    // $.ajax({
+    //     url : "http://localhost:8080/heap",
+    //     type : "get",
+    //     async : false,
+    //     dataType : "json",
+    //     success : function(result) {
+    //         console.log(result)
+    //         name = result.name;
+    //         value = result.value;
+    //         $('#name').html(name);
+    //         $('#value').html(value)
+    //         $('#quote').html(quote);
+    //     },
+    //     error : function(e){
+    //         console.log(e.status);
+    //         console.log(e.responseText);
+    //     }
+    // });
+
+    setTimeout(function () {
+        $('.box4').css('display', 'block');//1出现
+    }, 500);
+    setTimeout(function () {
+        $('.box4').css('margin-top', '180%');//1下降
+    }, 2000);
 }
 
 function getStack() {
@@ -53,25 +67,44 @@ function getStack() {
     var type = '';
     var value = '';
     var quote = '';
-    $.ajax({
-        url : "http://localhost:8080/stack",
-        type : "get",
-        async : false,
-        dataType : "json",
-        success : function(result) {
-            console.log(result)
-            variableName = result.variableName;
-            type = result.type;
-            value = result.value;
-            quote = result.quote;
-            $('#variableName').html(variableName);
-            $('#type').html(type);
-            $('#value').html(value);
-            $('#quote').html(quote);
-        },
-        error : function(e){
-            console.log(e.status);
-            console.log(e.responseText);
-        }
-    });
+    // $.ajax({
+    //     url : "http://localhost:8080/stack",
+    //     type : "get",
+    //     async : false,
+    //     dataType : "json",
+    //     success : function(result) {
+    //         console.log(result)
+    //         variableName = result.variableName;
+    //         type = result.type;
+    //         value = result.value;
+    //         quote = result.quote;
+    //         $('#variableName').html(variableName);
+    //         $('#type').html(type);
+    //         $('#value').html(value);
+    //         $('#quote').html(quote);
+    //     },
+    //     error : function(e){
+    //         console.log(e.status);
+    //         console.log(e.responseText);
+    //     }
+    // });
+
+    setTimeout(function () {
+        $('.box1').css('display', 'block');//1出现
+    }, 500);
+    setTimeout(function () {
+        $('.box1').css('margin-top', '180%');//1下降
+    }, 2000);
+    setTimeout(function () {
+        $('.box2').css('display', 'block');//堆2出现
+    }, 2500);
+    setTimeout(function () {
+        $('.box2').css('margin-top', '155%');
+    }, 4000);
+    setTimeout(function () {
+        $('.box3').css('display', 'block');//堆3出现
+    }, 4500);
+    setTimeout(function () {
+        $('.box3').css('margin-top', '130%');
+    }, 6000);
 }
