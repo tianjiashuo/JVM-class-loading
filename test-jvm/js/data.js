244ed76606fb17b9c1a692867f0db666e60b9038
BAI29 <1244283683@qq.com>
2020-10-13 14:28:14 +0800

    可视页面加载动画

--- test-jvm/js/data.js
+++ test-jvm/js/data.js
@@ -60,9 +60,149 @@ startClick = function () {
     }, 1950);
 }
 
+//随机生成div的颜色
+function randomColor(){
+    var temp = Math.floor(Math.random()*10);    // 可均衡获取 0 到 9 的随机整数。
+    var color = "";
+    if(temp < 3){
+        color = "#e1ca7280";
+    }else if(temp < 7){
+        color = "#9f819878";
+    }else{
+        color = "#7a99ad73";
+    }
+
+    return color;
+}
+
+//显示数据
+function showData(i,testMsg){
+    if(testMsg[i] != null){
+        switch(testMsg[i].area){
+            case "JvmStack":
+                //获取div
+                var div = document.getElementById("vmStackBody");
+                //添加子div
+                var div2 = document.createElement("div");
+                div2.id = i;
+                div2.innerHTML = testMsg[i].type  +" "+ testMsg[i].methodName +" "+testMsg[i].method + " "+testMsg[i].fieldName + " "+testMsg[i].value +"<br/>";
+                div2.style = "display:none; margin: auto; margin-bottom: 5px; width: 95%; font-size: 1.3em; border-radius: 5px; text-align: center;";
+                div2.style.background = randomColor();
+                div.appendChild(div2);
+                break;
+            case "MethodArea":
+                //获取div
+                var div = document.getElementById("methodAreaBody");
+                //添加子div
+                var div2 = document.createElement("div");
+                div2.id = i;
+                div2.innerHTML = testMsg[i].type  +" "+ testMsg[i].classname +" "+testMsg[i].args + "<br/>";
+                div2.style = "display:none;margin: auto; margin-bottom: 5px; width: 95%; font-size: 1.3em; border-radius: 5px; text-align: center;";
+                div2.style.background = randomColor();
+                div.appendChild(div2);
+                break;
+            case "Heap":
+                //获取div
+                var div = document.getElementById("heapBody");
+                //添加子div
+                var div2 = document.createElement("div");
+                div2.id = i;
+                div2.innerHTML = testMsg[i].type  +" "+ testMsg[i].name + " = " + testMsg[i].value + "<br/>";
+                div2.style = "display:none; margin: auto; margin-bottom: 5px; width: 95%; font-size: 1.3em; border-radius: 5px; text-align: center;";
+                div2.style.background = randomColor();
+                div.appendChild(div2);
+                break;
+            case "NMStack":break;
+            case "PCRegister":break;
+            default: console.log("ERROR!!!");break;
+        }
+    }else{
+        console.log("NULL!!!");
+    }
+}
+
 run = function () {
+    console.log("!!!!!!");
+
+    // 将代码传给后端
+    // var test = $('#code').val();
+    // // var test = document.getElementById("code").value;
+    // // var test = "int i = 0";
+    // console.log(test);
+    //
+    // $.ajax({
+    //     type : "post",
+    //     url : "http://localhost:8080/aaa",//servlet
+    //     contentType:"application/json",
+    //     data : {"code":test },
+    //     success : function(msg){
+    //         console.log(msg);
+    //         alert(msg);
+    //     },
+    //     error : function(){
+    //         alert("error");
+    //     },
+    // });
+
     $('#showPage').css('display', 'block');
     $('#inputCodePage').css('display', 'none');
+
+    var testMsg = {
+        "1": null,
+        "2": {
+            "area": "MethodArea",
+            "type": null,
+            "classname": "com/jvm/demo/source/Add",
+            "pool": [
+                0,
+                1
+            ],
+            "method": [
+                "addcd()",
+                "add()"
+            ],
+            "param": null,
+            "value": null,
+            "fieldName": null
+        },
+        "3": {
+            "area": "MethodArea",
+            "type": "int",
+            "classname": null,
+            "pool": null,
+            "method": null,
+            "param": null,
+            "value": "1",
+            "fieldName": "b"
+        },
+        "4": null,
+        "5": {
+            "area": "Heap",
+            "name": "c",
+            "value": "3",
+            "type": "int"
+        },
+        "6": {
+            "area": "JvmStack",
+            "referenceName": null,
+            "type": null,
+            "methodName": "add",
+            "args": null
+        }
+    }
+    var testMsgLen = 6;
+    var aniTime = 500;
+
+    setTimeout(function () {
+        for (var i = 1; i <= testMsgLen; i++) {
+            showData(i,testMsg);
+            // console.log("延迟开始");
+            $("#"+i).delay(aniTime).fadeIn();
+            // console.log("延迟结束");
+            aniTime += 500;
+        }
+    }, 100);
+
 }
 
 
