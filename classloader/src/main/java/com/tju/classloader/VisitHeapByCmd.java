a61c1b3002f4bc9e9193dda4043b7c4c2d530ee8
liqingying <>
2021-02-26 23:30:59 +0800

    堆信息获取

--- classloader/src/main/java/com/tju/classloader/VisitHeapByCmd.java
+++ classloader/src/main/java/com/tju/classloader/VisitHeapByCmd.java
@@ -7,38 +7,60 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 public class VisitHeapByCmd {
-        public static void exeCmd(String commandStr) {
-            BufferedReader br = null;
-            try {
-                Process p = Runtime.getRuntime().exec(commandStr);
-                br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String line = null;
-                StringBuilder sb = new StringBuilder();
-                while ((line = br.readLine()) != null) {
-                    sb.append(line + "\n");
-                }
-                System.out.println(sb.toString());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            finally
-            {
-                if (br != null)
-                {
-                    try {
-                        br.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
+    static String allPids = "";
 
-        public static void main(String[] args) {
-            String commandStr = "jps";
-            VisitHeapByCmd.exeCmd(commandStr);
-//            String commandStr1 = "jmap -heap 2772";
-//            VisitHeapByCmd.exeCmd(commandStr);
+    public static String exeCmd(String commandStr) {
+        BufferedReader br = null;
+        try {
+            Process p = Runtime.getRuntime().exec(commandStr);
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+//            System.out.println(sb.toString());
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        finally
+        {
+            if (br != null)
+            {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static String getPid(String allPids){
+        String line[] = allPids.split("\\r?\\n");
+        int index = 0;
+        for(int i = 0; i < line.length;i ++){
+            if(line[i].contains("VisitHeapByCmd")){
+                index = i;
+            }
+        }
+//        System.out.println(line[index]);
+        String targetPid = line[index];
+        String pid = targetPid.split("\\s+")[0];
+//        System.out.println(pid);
+        return pid;
+    }
+
+    public static void main(String[] args) {
+        String commandStr = "jps";
+        allPids = VisitHeapByCmd.exeCmd(commandStr);
+//        System.out.println(allPids);
+        String pid = getPid(allPids);
+        String commandStr1 = "jmap -heap " + pid;
+        String result = VisitHeapByCmd.exeCmd(commandStr1);
+        System.out.println(result);
+    }
 
 }
